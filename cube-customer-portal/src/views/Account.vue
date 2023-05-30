1b86dd3c621983cbe45965e0f461bd513338a8b0
Andy <511793@student.saxion.nl>
2023-05-30 13:43:27 +0200

    - Navigation drawer and toolbar added. - Routing is moved from account to Navigation

--- cube-customer-portal/src/views/Account.vue
+++ cube-customer-portal/src/views/Account.vue
@@ -1,20 +1,26 @@
 <template>
+  <Navigation/>
   <div class="account ma-5">
-    <h1>Dashboard. Welcome.</h1>
 
-    <ul>
-      <router-link style="text-decoration: none" :to="{name: 'tickets'}"><v-btn class="ma-3">Tickets</v-btn></router-link>
-      <router-link style="text-decoration: none" :to="{name: 'invoices'}"><v-btn>Invoices</v-btn></router-link>
-      <router-link style="text-decoration: none" :to="{name: 'services'}"><v-btn class="ma-3">Services</v-btn></router-link>
-      <router-link style="text-decoration: none" :to="{name: 'teams'}"><v-btn>Teams</v-btn></router-link>
-    </ul>
+<!--    <ul>-->
+<!--      <router-link style="text-decoration: none" :to="{name: 'tickets'}"><v-btn class="ma-3">Tickets</v-btn></router-link>-->
+<!--      <router-link style="text-decoration: none" :to="{name: 'invoices'}"><v-btn>Invoices</v-btn></router-link>-->
+<!--      <router-link style="text-decoration: none" :to="{name: 'services'}"><v-btn class="ma-3">Services</v-btn></router-link>-->
+<!--      <router-link style="text-decoration: none" :to="{name: 'teams'}"><v-btn>Teams</v-btn></router-link>-->
+<!--    </ul>-->
 
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navigation from '/components/Navigation.vue'
+
 export default{
+  name: 'App',
+  components: {
+    Navigation
+  }
 }
 </script>
 
