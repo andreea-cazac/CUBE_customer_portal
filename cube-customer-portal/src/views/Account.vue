12f146dd7398aab1392ff8028ba04f6c0c2acd91
Marina Soboleva <505227@student.saxion.nl>
2023-06-01 21:02:38 +0200

    Dashboard design is done, buttons functionality added.

--- cube-customer-portal/src/views/Account.vue
+++ cube-customer-portal/src/views/Account.vue
@@ -1,14 +1,6 @@
 <template>
   <Navigation/>
   <div class="account ma-5">
-
-<!--    <ul>-->
-<!--      <router-link style="text-decoration: none" :to="{name: 'tickets'}"><v-btn class="ma-3">Tickets</v-btn></router-link>-->
-<!--      <router-link style="text-decoration: none" :to="{name: 'invoices'}"><v-btn>Invoices</v-btn></router-link>-->
-<!--      <router-link style="text-decoration: none" :to="{name: 'services'}"><v-btn class="ma-3">Services</v-btn></router-link>-->
-<!--      <router-link style="text-decoration: none" :to="{name: 'teams'}"><v-btn>Teams</v-btn></router-link>-->
-<!--    </ul>-->
-
     <router-view></router-view>
   </div>
 </template>
