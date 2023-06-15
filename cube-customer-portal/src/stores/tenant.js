1adfbae6934489ffc645364ac46fa733309276a4
Andreea Cazac <524141@student.saxion.nl>
2023-06-16 01:35:32 +0200

    tenantFavicon and tenantData persists after page reloading

--- cube-customer-portal/src/stores/tenant.js
+++ cube-customer-portal/src/stores/tenant.js
@@ -4,28 +4,19 @@ export const useTenantStore = defineStore('tenant', {
     // unique id of the store across your application
 
     state: () => ({
-        tenant:{
-            cubeUrl: "",
-            settings: {
-            primary_color: '',
-            accent_color: "",
-            logo: "",
-            favicon: "",
-            banner: "",
-            backgroundImage: ""
-        }}
+        tenant:
+            JSON.parse(localStorage.getItem("tenant")) ||
+            {},
     }),
+
     getters: {
-        cubeUrl: (state) => state.cubeUrl,
-        primaryColor: (state) => state.settings.primary_color,
-        logo: (state) => state.settings.logo,
-        favicon: (state) => state.settings.favicon,
-        banner: (state) => state.settings.banner,
-        backgroundImage: (state) => state.settings.backgroundImage,
+        getTenant: state => state.tenant
     },
+
     actions: {
         setTenant(tenant){
             this.tenant = tenant;
+            localStorage.setItem("tenant", JSON.stringify(tenant));
         }
     }
 });
