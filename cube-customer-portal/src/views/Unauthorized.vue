4e525d0ca4610bb626e41ab255f5868a5b2e8055
Andreea Cazac <524141@student.saxion.nl>
2023-06-22 18:46:25 +0200

    US4. Navigation Guard for all routes except login was added

---
+++ cube-customer-portal/src/views/Unauthorized.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container fluid fill-height class="d-flex flex-column justify-center align-center">
+    <v-row justify="center">
+      <v-col cols="12" sm="10" md="8">
+        <v-card color="red lighten-5" class="pa-6" outlined>
+          <v-card-title class="headline display-2">401 Unauthorized</v-card-title>
+          <v-card-text class="title">
+            Sorry, you are unauthorized for accessing this route!
+            Please go to the
+            <router-link to="/login">login page</router-link>
+            and try to login.
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Unauthorized",
+}
+</script>
