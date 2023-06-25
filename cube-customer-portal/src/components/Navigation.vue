7fbd811576fdd8df44d12131b9c61d949098a4e8
Andy <511793@student.saxion.nl>
2023-06-25 21:41:23 +0200

    - added account-details-deletion.js - timestamp is taken when user logs - if 8 hours have passed, account data is cleared (in routing, beforeEach) - if a client API fetch fails (wrong token), the previous account data gets deleted and the user redirected to error page

--- cube-customer-portal/src/components/Navigation.vue
+++ cube-customer-portal/src/components/Navigation.vue
@@ -1,77 +1,81 @@
 <template>
 
-    <nav>
-        <v-navigation-drawer v-model="drawer" color="white">
-            <v-row no-gutters>
-                <v-col class="mt-6 text-center">
-                    <v-img v-bind:src="logo" alt="logo"></v-img>
-                </v-col>
-            </v-row>
+  <nav>
+    <v-navigation-drawer v-model="drawer" color="white">
+      <v-row no-gutters>
+        <v-col class="mt-6 text-center">
+          <v-img v-bind:src="logo" alt="logo"></v-img>
+        </v-col>
+      </v-row>
 
 
-            <v-list v-if="permittedPages" v-bind:color="primary_color">
-                <v-list-item
-                        v-for="link in permittedPages"
-                        :key="link.text"
-                        router
-                        :to="link.route">
-                    <v-list-item-action class="active">
-                        <v-icon color="grey-darken-3" class="mx-3">{{ link.icon }}</v-icon>
-                        <v-list-item-title color="grey-darken-3">{{ $t(link.textKey) }}</v-list-item-title>
-                    </v-list-item-action>
-                </v-list-item>
-            </v-list>
-        </v-navigation-drawer>
+      <v-list v-if="permittedPages" v-bind:color="primary_color">
+        <v-list-item
+            v-for="link in permittedPages"
+            :key="link.text"
+            router
+            :to="link.route">
+          <v-list-item-action class="active">
+            <v-icon color="grey-darken-3" class="mx-3">{{ link.icon }}</v-icon>
+            <v-list-item-title color="grey-darken-3">{{ $t(link.textKey) }}</v-list-item-title>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
-        <v-toolbar v-bind:color="primary_color">
-            <v-app-bar-nav-icon :class="`${colorCalculation(primary_color)}`" @click="drawer = !drawer">
-            </v-app-bar-nav-icon>
+    <v-toolbar v-bind:color="primary_color">
+      <v-app-bar-nav-icon :class="`${colorCalculation(primary_color)}`" @click="drawer = !drawer">
+      </v-app-bar-nav-icon>
 
-            <v-toolbar-title :class="`${colorCalculation(primary_color)}`">
-                <span class="font-weight-light">{{ $t('welcomeMessage') }}</span>
-            </v-toolbar-title>
-
-
-
-            <v-menu location="bottom">
-                <template v-slot:activator="{ props }">
-                    <v-btn variant="flat" v-bind:color="primary_color" :class="`${colorCalculation(primary_color)}`" size="small" v-bind="props" >
-                        <span v-if="activeRelation" class="d-none d-sm-flex ma-2">{{ activeRelation.name }}</span>
-                        <v-icon class="ma-1">mdi-account-switch-outline</v-icon>
-                    </v-btn>
-                </template>
-                <v-list-item class="bg-white border" v-for="(item, index) in relations" :key="index" @click="selectRelation(item)" >
-                    <v-list-item-title>{{ item.name }}</v-list-item-title>
-                </v-list-item>
-            </v-menu>
+      <v-toolbar-title :class="`${colorCalculation(primary_color)}`">
+        <span class="font-weight-light">{{ $t('welcomeMessage') }}</span>
+      </v-toolbar-title>
 
 
-            <v-menu location="bottom">
-                <template v-slot:activator="{ props }">
-                    <v-btn variant="flat" v-bind:color="primary_color" :class="`${colorCalculation(primary_color)}`" size="small" v-bind="props" >
-                        <span class=" d-none d-sm-flex ma-2">{{ $t('active_language') }} </span>
-                        <v-icon class="ma-1">mdi-translate</v-icon>
-                    </v-btn>
-                </template>
-                <v-list-item class="bg-white border" v-for="language in languages" :key="language.code" @click="changeLanguage(language.code)">
-                    <v-list-item-title>{{ language.title }}</v-list-item-title>
-                </v-list-item>
+      <v-menu location="bottom">
+        <template v-slot:activator="{ props }">
+          <v-btn variant="flat" v-bind:color="primary_color" :class="`${colorCalculation(primary_color)}`" size="small"
+                 v-bind="props">
+            <span v-if="activeRelation" class="d-none d-sm-flex ma-2">{{ activeRelation.name }}</span>
+            <v-icon class="ma-1">mdi-account-switch-outline</v-icon>
+          </v-btn>
+        </template>
+        <v-list-item class="bg-white border" v-for="(item, index) in relations" :key="index"
+                     @click="selectRelation(item)">
+          <v-list-item-title>{{ item.name }}</v-list-item-title>
+        </v-list-item>
+      </v-menu>
 
-            </v-menu>
+
+      <v-menu location="bottom">
+        <template v-slot:activator="{ props }">
+          <v-btn variant="flat" v-bind:color="primary_color" :class="`${colorCalculation(primary_color)}`" size="small"
+                 v-bind="props">
+            <span class=" d-none d-sm-flex ma-2">{{ $t('active_language') }} </span>
+            <v-icon class="ma-1">mdi-translate</v-icon>
+          </v-btn>
+        </template>
+        <v-list-item class="bg-white border" v-for="language in languages" :key="language.code"
+                     @click="changeLanguage(language.code)">
+          <v-list-item-title>{{ language.title }}</v-list-item-title>
+        </v-list-item>
+
+      </v-menu>
 
 
-            <v-btn v-bind:color="primary_color" :class="`${colorCalculation(primary_color)}`" :key="logout" router :to="loginRoute">
-                <span class="d-none d-sm-flex"  @click="logout()" >{{ $t('signOut') }}</span>
-                <v-icon>mdi-exit-to-app</v-icon>
-            </v-btn>
-        </v-toolbar>
-    </nav>
+      <v-btn v-bind:color="primary_color" :class="`${colorCalculation(primary_color)}`" :key="logout" router
+             :to="loginRoute">
+        <span class="d-none d-sm-flex" @click="logout()">{{ $t('signOut') }}</span>
+        <v-icon>mdi-exit-to-app</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </nav>
 </template>
 
 <script>
 
 import {useActiveRelationStore} from '@/stores/activeRelation.js';
-import {computed, ref} from "vue";
+import {ref} from "vue";
 import {useUserRelationsStore} from "@/stores/userRelationsStore";
 import {useUserStore} from "@/stores/userStore";
 import {useTenantStore} from '../stores/tenant';
